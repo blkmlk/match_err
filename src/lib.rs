a057a039ea19ae26851314efd28d120cf918da28
Islam Bekbuzarov <n157318@gmail.com>
2024-08-01 23:33:09 +0300

    added doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,25 @@
+/// Matches an error against an enum-like error type by hiding the usage of downcast_ref method
+///
+/// # Examples
+/// ```
+///  use match_err::match_err;
+///
+///  #[derive(thiserror::Error, Debug)]
+///  enum Error {
+///     #[error("not found")]
+///     NotFound,
+///     #[error("custom: {0}")]
+///     Custom(String),
+///  }
+///
+///  let err: Result<(), _> = Err(anyhow!(Error::NotFound));
+///
+///  match_err!(err, Error, {
+///     NotFound => println!("not found"),
+///     Custom(msg) => println!("custom message: {}", msg),
+///     _ => println!("unknown")
+///  })
+/// ```
 #[macro_export]
 macro_rules! match_err {
     ( $any:expr, $ty:ident, { $( $variant:ident $( ( $($inner:ident),* ) )? => $arm:expr ),*, _ => $default:expr } ) => (
@@ -16,6 +38,7 @@ macro_rules! match_err {
             $default
         }
     );
+
     ( $any:expr, $ty:ident, { $( $variant:ident $( ( $($inner:ident),* ) )? => $arm:expr ),* $(,)? }) => (
         match_err!($any, $ty, { $( $variant $( ( $($inner),* ) )? => $arm ),*, _ => {} })
     );
