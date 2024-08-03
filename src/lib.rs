ef145573f23024765b6f70477e6a0fcfa2c6e21c
Islam Bekbuzarov <n157318@gmail.com>
2024-08-03 08:34:13 +0300

    updated homepage

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,41 @@
+//! # match_err
+//!
+//! Macro for quick matching errors against enum-like error types
+//!
+//! Helps to avoid writing long and tedious structures like:
+//! ```rust
+//! if let Err(e) = err {
+//!     if let Some(e) = e.downcast_ref::<Error>() {
+//!         match e {
+//!         ...
+//!         }
+//!     }
+//! }
+//! ```
+//!
+//! ## Examples
+//!
+//! ```rust
+//! use match_err::*;
+//!
+//! #[derive(thiserror::Error, Debug)]
+//! enum Error {
+//!     #[error("not found")]
+//!     NotFound,
+//!     #[error("custom: {0}")]
+//!     Custom(String),
+//! }
+//!
+//! let err: Result<(), _> = Err(anyhow!(Error::NotFound));
+//!
+//! match_if_err!(err, Error, {
+//!     NotFound => println!("not found"),
+//!     Custom(msg) => println!("custom message: {}", msg),
+//!     _ => println!("unknown")
+//! })
+//! ```
+
+
 /// Matches an error against an enum-like error type by hiding the usage of downcast_ref method
 ///
 /// # Examples
